de0968b5d9a02b8129f8952ac10747d4a5c3d62b
Code-digga <wildbase22@gmail.com>
2024-05-19 13:39:42 +0100

    chore: building school calendar page

---
+++ lib/screens/school_calendar/school_calendar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+class Calendar extends StatelessWidget {
+  const Calendar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformScaffold();
+  }
+}
